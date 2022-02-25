c84929c2bc13f96149981952b96b6b222826fa58
chuvoddolyhood <trannhannghia.24012000@gmail.com>
2022-02-25 22:25:37 +0700

    completed to add pageview follow bottom nav

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,14 +9,62 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late PageController pageController;
+  late int selectedTabNav = 0;
+
+  @override
+  void initState() {
+    pageController = PageController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Text('content'),
-          BottomNavigator_Widget(),
+          Expanded(
+              child: PageView(
+            controller: pageController,
+            onPageChanged: (index) {
+              setState(() {
+                selectedTabNav = index;
+              });
+            },
+            children: [
+              Container(
+                // color: Colors.red,
+                child: Center(
+                  child: Text('Home Page'),
+                ),
+              ),
+              Container(
+                child: Center(
+                  child: Text('Search Page'),
+                ),
+              ),
+              Container(
+                child: Center(
+                  child: Text('Infor Page'),
+                ),
+              )
+            ],
+          )),
+          BottomNavigator_Widget(
+            selectedTab: selectedTabNav,
+            tabPressed: (index) {
+              pageController.animateToPage(index,
+                  duration: Duration(milliseconds: 300),
+                  curve: Curves.fastLinearToSlowEaseIn);
+            },
+          ),
         ],
       ),
     );
--- lib/widgets/bottom_nav_widget.dart
+++ lib/widgets/bottom_nav_widget.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class BottomNavigator_Widget extends StatefulWidget {
-  const BottomNavigator_Widget({Key? key}) : super(key: key);
+  final int selectedTab;
+  final Function(int) tabPressed;
+
+  const BottomNavigator_Widget(
+      {Key? key, required this.selectedTab, required this.tabPressed})
+      : super(key: key);
 
   @override
   State<BottomNavigator_Widget> createState() => _BottomNavigator_WidgetState();
@@ -12,30 +18,42 @@ class _BottomNavigator_WidgetState extends State<BottomNavigator_Widget> {
 
   @override
   Widget build(BuildContext context) {
+    selectTab = widget.selectedTab;
+    double displayWidth = MediaQuery.of(context).size.width;
     return Container(
-      padding: EdgeInsets.symmetric(vertical: 10),
-      margin: EdgeInsets.symmetric(vertical: 20, horizontal: 70),
+      padding: EdgeInsets.symmetric(vertical: displayWidth * 0.03),
+      // margin: EdgeInsets.symmetric(vertical: 20, horizontal: 70),
       decoration: BoxDecoration(
           color: Colors.white,
           boxShadow: [
             BoxShadow(
                 color: Colors.black26, blurRadius: 10.0, spreadRadius: 1.0)
           ],
-          borderRadius: BorderRadius.circular(30)),
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(15), topRight: Radius.circular(15))),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           ButtonNav_Widget(
             titleIcon: 'home',
             isSelected: (selectTab == 0 ? true : false),
+            onTap_: () {
+              widget.tabPressed(0);
+            },
           ),
           ButtonNav_Widget(
             titleIcon: 'search',
             isSelected: (selectTab == 1 ? true : false),
+            onTap_: () {
+              widget.tabPressed(1);
+            },
           ),
           ButtonNav_Widget(
             titleIcon: 'about',
             isSelected: (selectTab == 2 ? true : false),
+            onTap_: () {
+              widget.tabPressed(2);
+            },
           ),
         ],
       ),
@@ -46,34 +64,47 @@ class _BottomNavigator_WidgetState extends State<BottomNavigator_Widget> {
 class ButtonNav_Widget extends StatelessWidget {
   final String titleIcon;
   final bool isSelected;
+  final VoidCallback onTap_;
   const ButtonNav_Widget(
-      {Key? key, required this.titleIcon, required this.isSelected})
+      {Key? key,
+      required this.titleIcon,
+      required this.isSelected,
+      required this.onTap_})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     if (titleIcon == 'home') {
       return Container(
-        child: Icon(
-          Icons.home_outlined,
-          size: 30,
-          color: isSelected ? Colors.black : Colors.grey,
+        child: InkWell(
+          onTap: onTap_,
+          child: Icon(
+            Icons.home_outlined,
+            size: 30,
+            color: isSelected ? Colors.black : Colors.grey,
+          ),
         ),
       );
     } else if (titleIcon == 'search') {
       return Container(
-        child: Icon(
-          Icons.search,
-          size: 30,
-          color: isSelected ? Colors.black : Colors.grey,
+        child: InkWell(
+          onTap: onTap_,
+          child: Icon(
+            Icons.search,
+            size: 30,
+            color: isSelected ? Colors.black : Colors.grey,
+          ),
         ),
       );
     } else if (titleIcon == 'about') {
       return Container(
-        child: Icon(
-          Icons.info_outlined,
-          size: 30,
-          color: isSelected ? Colors.black : Colors.grey,
+        child: InkWell(
+          onTap: onTap_,
+          child: Icon(
+            Icons.info_outlined,
+            size: 30,
+            color: isSelected ? Colors.black : Colors.grey,
+          ),
         ),
       );
     }
